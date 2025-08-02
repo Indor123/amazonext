5612f098006c5892a07d16f1cf23ee18af0fb6cf
Indor <dasindor3@gmail.com>
2025-08-02 22:37:50 +0530

    fix: restored project structure from backup zip

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: Center(child: Text("Amazonext App"))),
+      home: const Scaffold(
+        body: Center(child: Text('Welcome to Amazonext!')),
+      ),
     );
   }
 }
