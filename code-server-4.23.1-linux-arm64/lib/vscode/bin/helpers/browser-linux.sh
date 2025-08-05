7fe0dc88419d41b3054dadab339c0b1e5d82ea7d
Indor <dasindor3@gmail.com>
2025-08-06 03:08:27 +0530

    Add test workflow

---
+++ code-server-4.23.1-linux-arm64/lib/vscode/bin/helpers/browser-linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+VSROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+ROOT="$(dirname "$(dirname "$VSROOT")")"
+
+APP_NAME="code-server"
+VERSION="1.88.1"
+COMMIT="9a28bc29dbddb6886dfe03dc1c31320249a901ce"
+EXEC_NAME="code-server"
+CLI_SCRIPT="$VSROOT/out/server-cli.js"
+"${NODE_EXEC_PATH:-$ROOT/lib/node}" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
