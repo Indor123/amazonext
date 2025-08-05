2fdff21a9a8d941b8a07bf9f8bb1ac087f2b7623
Indor <dasindor3@gmail.com>
2025-08-06 04:04:24 +0530

    Trigger test run with flutter-test.yml

--- code-server-4.23.1-linux-arm64/lib/vscode/bin/helpers/browser.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set ROOT_DIR=%~dp0..\..\..\..
-set VSROOT_DIR=%~dp0..\..
-call "%ROOT_DIR%\node.exe" "%VSROOT_DIR%\out\server-cli.js" "code-server" "1.88.1" "9a28bc29dbddb6886dfe03dc1c31320249a901ce" "code-server.cmd" "--openExternal" %*
-endlocal
